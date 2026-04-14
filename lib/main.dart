3854e30d651bf7bb4b6d900c254e672b192a2613
kalafrio2010 <kalafrio2010@gmail.com>
2026-04-14 23:05:47 +0000

    Semana 7

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+  void _decrementCounter() {
+  setState(() {
+    _counter--;
+  });
+}
 
   @override
   Widget build(BuildContext context) {
@@ -103,20 +108,23 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+         children: <Widget>[
+  const Text('You have pushed the button this many times:'),
+  Text(
+    '$_counter',
+    style: Theme.of(context).textTheme.headlineMedium,
+  ),
+  TextButton(
+    onPressed: _incrementCounter,
+    child: const Text('Increment'),
+  ),
+  TextButton(
+    onPressed: _decrementCounter,
+    child: const Text('Decrement'),
+  ),
+],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
